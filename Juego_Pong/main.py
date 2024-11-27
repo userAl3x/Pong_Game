1f26508cbddeefc930ab6e512be098d5101c4ad5
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2024-11-27 16:37:37 +0100

    Update main.py
    Se implementa el inicio del pygame, la configuración de la pantalla y los colores del jeugo

--- Juego_Pong/main.py
+++ Juego_Pong/main.py
@@ -4,3 +4,16 @@ import sys
 # Autor: Alex Jiménez Quiñonero
 # Ejercicio: Programem jocs amb Python
 # Fecha: 27/11/2024
+
+# Iniciamos el Pygame
+pygame.init()
+
+# Configuración de la pantalla
+SCREEN_WIDTH = 900  # Anchura
+SCREEN_HEIGHT = 600  # Altura
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Juego Pong by Alex JQ")  # Título del Juego
+
+# Colores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
