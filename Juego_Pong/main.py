83a44e8df1528a22062e206c3e771df38a12b672
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2024-11-27 16:39:35 +0100

    Update main.py
    Implementación de Dimensiones y Velocidades de la Paletas y la Bola

--- Juego_Pong/main.py
+++ Juego_Pong/main.py
@@ -17,3 +17,13 @@ pygame.display.set_caption("Juego Pong by Alex JQ")  # Título del Juego
 # Colores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+# Dimensiones de la paleta y la bola
+PADDLE_WIDTH = 10  # Raqueta
+PADDLE_HEIGHT = 100
+BALL_SIZE = 20  # Bola
+
+# Velocidades de la bola y las raquetas (Paddle)
+BALL_SPEED_X = 7  # Bola
+BALL_SPEED_Y = 7
+PADDLE_SPEED = 7  # Raqueta
